625519ebaaa83648565ab6382a3e46b34935df7b
Dawcio <dawid.palgan@gmail.com>
2024-05-04 22:27:26 +0100

    changed eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,9 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/documents');
     eleventyConfig.addPassthroughCopy('./src/js/script.js');
-
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
     return {
         dir: {
